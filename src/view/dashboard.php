055163383ccaef42a681f07e5a99af4999bc2568
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-03-19 14:32:04 +0800

    Finished the styling of navigation and dashboard page

--- src/view/dashboard.php
+++ src/view/dashboard.php
@@ -4,68 +4,69 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Café de Franz</title>
   <!-- Tailwind cdn -->
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
   <!-- Flowbite cdn -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+  <!-- General css -->
+  <link rel="stylesheet" href="../../assets/css/style.css">
 </head>
 <!-- Admin side and navigation bar -->
 <?php
 include_once './navigation.php';
 ?>
 
-<body class="bg-gray-900">
-
+<body>
   <div class="p-6 sm:ml-64">
     <!-- Main container -->
-    <div class="border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 h-auto space-y-5">
+    <div class="rounded-lg dark:border-gray-700 h-auto space-y-5">
       <!-- Page content -->
       <div class="grid grid-cols-2 gap-5">
         <!-- Stock and expired items container -->
-        <div class="flex items-center justify-center h-auto rounded bg-gray-50 dark:bg-gray-800 p-8 flex-col space-y-5">
-          <div class="flex items-center justify-between p-5 w-full bg-white border border-gray-200 rounded-lg shadow border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-            <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Stock Items</h5>
-            <p class="font-bold text-gray-900 dark:text-gray-400">422</p>
+        <div class="flex items-center justify-center h-auto rounded-lg p-8 flex-col space-y-5 container-shadow">
+          <div class="flex items-center justify-between p-5 w-full cards rounded-lg px-8">
+            <h5 class="text-2xl font-bold tracking-tight">Stock Items</h5>
+            <p class="font-bold">422</p>
           </div>
-          <div class="flex items-center justify-between p-5 w-full bg-white border border-gray-200 rounded-lg shadow border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-            <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Expired Items</h5>
-            <p class="font-bold text-gray-900 dark:text-gray-400">35</p>
+          <div class="flex items-center justify-between p-5 w-full cards rounded-lg px-8">
+            <h5 class="text-2xl font-bold tracking-tight">Expired Items</h5>
+            <p class="font-bold">35</p>
           </div>
-
         </div>
         <div class="flex justify-center h-auto rounded flex-col space-y-4">
           <!-- Time and Date -->
-          <div class="flex items-center space-x-4 p-5 w-full h-full bg-gray-50 dark:bg-gray-800 rounded">
+          <div class="flex items-center space-x-4 p-5 w-full h-full container-shadow rounded-lg">
             <!-- Clock Icon -->
-            <svg class="w-14 h-14 mx-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <svg class="w-14 h-14 mx-6 container-text" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
             <div>
               <!-- Time -->
-              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Time</label>
-              <span class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">11:45am</span>
+              <label class="block mb-2 text-sm font-medium container-text">Time</label>
+              <span class="time-date text-sm rounded-lg block w-full p-2.5">11:45am</span>
             </div>
             <div>
               <!-- Date -->
-              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
-              <span class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">03/18/2024</span>
+              <label class="block mb-2 text-sm font-medium container-text">Date</label>
+              <span class="time-date text-sm rounded-lg block w-full p-2.5">03/18/2024</span>
             </div>
           </div>
           <!-- Lists -->
-          <div class="flex items-center justify-between p-5 pt-14 w-full h-40 bg-gray-50 dark:bg-gray-800 rounded overflow-y-auto">
+          <div class="flex items-center justify-between p-5 pt-14 w-full h-40 container-shadow rounded-lg overflow-y-auto px-8">
             <ul class="space-y-1 text-gray-500 list-none list-inside dark:text-gray-400">
-              <li>
-                <span class="text-white font-medium">Admin: </span>Update a product: Coffee Grounds: 2 Packs
+              <li class="container-text">
+                <span class="container-text font-bold">Admin: </span>Update a product: Coffee Grounds: 2 Packs
               </li>
-              <li>
-                <span class="text-white font-medium">Admin: </span>Deleted expired product: Fresh Milk
-              <li>
-                <span class="text-white font-medium">Admin: </span>Update a product: Coffee Grounds: 2 packs
+              <li class="container-text">
+                <span class="container-text font-bold">Admin: </span>Deleted expired product: Fresh Milk
               </li>
-              <li>
-                <span class="text-white font-medium">Admin: </span>Update a product: Coffee Grounds: 2 packs
+              <li class="container-text">
+                <span class="container-text font-bold">Admin: </span>Update a product: Coffee Grounds: 2 packs
+              </li>
+              <li class="container-text">
+                <span class="container-text font-bold">Admin: </span>Update a product: Coffee Grounds: 2 packs
               </li>
             </ul>
           </div>
@@ -73,11 +74,11 @@ include_once './navigation.php';
       </div>
       <!--  -->
       <div class="grid grid-cols-2 gap-5">
-        <!-- Stock and expired items container -->
-        <div class="flex items-center justify-center h-96 rounded bg-gray-50 dark:bg-gray-800 p-8 flex-col space-y-5">
+        <!-- 2 container for graph -->
+        <div class="flex items-center justify-center h-96 rounded-lg container-shadow p-8 flex-col space-y-5">
 
         </div>
-        <div class="flex items-center justify-center h-96 rounded bg-gray-50 dark:bg-gray-800 p-8 flex-col space-y-5">
+        <div class="flex items-center justify-center h-96 rounded-lg container-shadow p-8 flex-col space-y-5">
 
         </div>
       </div>
--- src/view/navigation.php
+++ src/view/navigation.php
@@ -10,9 +10,29 @@
   <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
   <!-- Flowbite cdn -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+  <!-- Tailwind Colors -->
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            sideBar: '#d1b08b',
+            darkText: '#401e0b',
+            logoutBtn: '#fbead4',
+            logoutBtnHover: '#f4cfa2',
+            primaryBtn: '#8e5e44',
+            background: '#fef0db',
+            borderRing: '#8e7555',
+            navHover: '#b0926b',
+            navOutline: '#b89d76',
+          }
+        }
+      }
+    }
+  </script>
 </head>
 
-<body class="bg-gray-900">
+<body class="bg-background">
   <!-- Burger for sidebar -->
   <button data-drawer-target="separator-sidebar" data-drawer-toggle="separator-sidebar" aria-controls="separator-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
     <span class="sr-only">Open sidebar</span>
@@ -23,62 +43,67 @@
   </button>
   <!-- Sidebar -->
   <aside id="separator-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-    <div class="h-full px-3 py-8 overflow-y-auto bg-gray-50 dark:bg-gray-800 flex flex-col justify-between items-center">
+    <div class="h-full px-3 py-8 overflow-y-auto bg-sideBar flex flex-col justify-between items-center">
       <!-- Upper Side -->
       <div class="flex flex-col items-center space-y-6">
-        <div class="border border-solid border-white w-28 h-28"></div>
-        <h5 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Hello Admin!</h5><br>
-        <h5 class="text-lg font-medium tracking-tight text-gray-900 dark:text-white">Inventory System</h5>
+        <img src="../../assets/image/store-logo.png" class="w-44 h-auto" alt="Cafe de Franz">
+        <h5 class="text-3xl font-bold tracking-tight text-darkText">Hello Admin!</h5>
+        <h5 class="text-lg font-medium tracking-tight text-darkText">Inventory System</h5>
       </div>
       <!-- Lower Side -->
       <div class="flex flex-col items-center space-y-4">
         <!-- Notification Bell -->
-        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <svg class="w-6 h-6 text-darkText" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
           <path d="M17.133 12.632v-1.8a5.407 5.407 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.933.933 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175Zm-13.267-.8a1 1 0 0 1-1-1 9.424 9.424 0 0 1 2.517-6.391A1.001 1.001 0 1 1 6.854 5.8a7.43 7.43 0 0 0-1.988 5.037 1 1 0 0 1-1 .995Zm16.268 0a1 1 0 0 1-1-1A7.431 7.431 0 0 0 17.146 5.8a1 1 0 0 1 1.471-1.354 9.424 9.424 0 0 1 2.517 6.391 1 1 0 0 1-1 .995ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z" />
         </svg>
         <!-- Logout Button -->
-        <button type="button" class="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          <svg class="w-3 h-3 mr-3   text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <button type="button" class="px-7 py-3 text-sm font-medium text-center inline-flex items-center text-darkText bg-logoutBtn rounded-lg hover:bg-logoutBtnHover focus:ring-4 focus:outline-none focus:ring-primaryBtn">
+          <svg class="w-3 h-3 mr-3 text-darkText" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2" />
           </svg>
           Logout
         </button>
-
       </div>
     </div>
   </aside>
   <!-- Navigation bar -->
-  <div class="rounded-md shadow-sm border border-black border-solid grid grid-cols-5 lg:ml-64 md:ml-64 px-5 pt-5" role="group">
-    <button type="button" class="inline-flex justify-center items-center py-5 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
-      <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+  <div class="grid grid-cols-5 lg:ml-64 md:ml-64 px-5 pt-5" role="group">
+    <!-- Dashboard -->
+    <button type="button" class="inline-flex justify-center items-center py-5 text-sm font-medium text-darkText bg-sideBar border border-navOutline rounded-s-lg hover:bg-navHover hover:text-white focus:z-10 focus:ring-4 focus:ring-borderRing focus:bg-navHover focus:text-white">
+      <svg class="w-5 h-5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M4.857 3A1.857 1.857 0 0 0 3 4.857v4.286C3 10.169 3.831 11 4.857 11h4.286A1.857 1.857 0 0 0 11 9.143V4.857A1.857 1.857 0 0 0 9.143 3H4.857Zm10 0A1.857 1.857 0 0 0 13 4.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 21 9.143V4.857A1.857 1.857 0 0 0 19.143 3h-4.286Zm-10 10A1.857 1.857 0 0 0 3 14.857v4.286C3 20.169 3.831 21 4.857 21h4.286A1.857 1.857 0 0 0 11 19.143v-4.286A1.857 1.857 0 0 0 9.143 13H4.857Zm10 0A1.857 1.857 0 0 0 13 14.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 21 19.143v-4.286A1.857 1.857 0 0 0 19.143 13h-4.286Z" clip-rule="evenodd" />
       </svg>
-      Profiles
+      Dashboard
     </button>
-    <button type="button" class="inline-flex justify-center items-center text-sm font-medium text-gray-900 bg-white border-t border-b border-l border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
-      <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
+    <!-- Product Management -->
+    <button type="button" class="inline-flex justify-center items-center py-5 text-sm font-medium text-darkText bg-sideBar border-t border-b border-l border-navOutline hover:bg-navHover hover:text-white focus:z-10 focus:ring-4 focus:ring-borderRing focus:bg-navHover focus:text-white">
+      <svg class="w-5 h-5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M20 10H4v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8ZM9 13v-1h6v1a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1Z" clip-rule="evenodd" />
+        <path d="M2 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2Z" />
       </svg>
-      Settings
+      Product Management
     </button>
-    <button type="button" class="inline-flex justify-center items-center  text-sm font-medium text-gray-900 bg-white border-t border-b border-l border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
-      <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
+    <!-- Product List -->
+    <button type="button" class="inline-flex justify-center items-center py-5 text-sm font-medium text-darkText bg-sideBar border-t border-b border-l border-navOutline hover:bg-navHover hover:text-white focus:z-10 focus:ring-4 focus:ring-borderRing focus:bg-navHover focus:text-white">
+      <svg class="w-5 h-5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M8 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1h2a2 2 0 0 1 2 2v15a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h2Zm6 1h-4v2H9a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2h-1V4Zm-3 8a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-2-1a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H9Zm2 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-2-1a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H9Z" clip-rule="evenodd" />
       </svg>
-      Settings
+      Product List
     </button>
-    <button type="button" class="inline-flex justify-center items-center text-sm font-medium text-gray-900 bg-white border-t border-b border-l border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
-      <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
+    <!-- Supplier Button -->
+    <button type="button" class="inline-flex justify-center items-center py-5 text-sm font-medium text-darkText bg-sideBar border-t border-b border-l border-navOutline hover:bg-navHover hover:text-white focus:z-10 focus:ring-4 focus:ring-borderRing focus:bg-navHover focus:text-white">
+      <svg class="w-5 h-5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z" clip-rule="evenodd" />
       </svg>
-      Settings
+      Supplier
     </button>
-    <button type="button" class="inline-flex justify-center items-center text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
-      <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
-        <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
+    <!-- Logs -->
+    <button type="button" class="inline-flex justify-center items-center py-5 text-sm font-medium text-darkText bg-sideBar border rounded-e-lg border-navOutline hover:bg-navHover hover:text-white focus:z-10 focus:ring-4 focus:ring-borderRing focus:bg-navHover focus:text-white">
+      <svg class="w-5 h-5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M7 2a2 2 0 0 0-2 2v1a1 1 0 0 0 0 2v1a1 1 0 0 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H7Zm3 8a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm-1 7a3 3 0 0 1 3-3h2a3 3 0 0 1 3 3 1 1 0 0 1-1 1h-6a1 1 0 0 1-1-1Z" clip-rule="evenodd" />
       </svg>
-      Downloads
+
+      Logs
     </button>
   </div>
 
