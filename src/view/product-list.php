ec7a3f10524f0ad11c2c29af0939aa8c73dafd2b
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-03-19 23:57:41 +0800

    Modified the navigation links and their appropriate page titles

--- src/view/product-list.php
+++ src/view/product-list.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Café de Franz</title>
+  <title>Café de Franz | Product List</title>
   <!-- Tailwind cdn -->
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
